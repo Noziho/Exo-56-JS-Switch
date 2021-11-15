1b7160683fd400624edc3d1c4d04327595b71412
Noah2211 <noah.decroix3@gmail.com>
2021-11-15 10:20:21 +0100

    finished exercices

---
+++ app.js
@@ -0,0 +1,31 @@
+let varia1 = prompt ("accessoire informatique:")
+
+switch (varia1) {
+
+    case 'souris' :
+        alert ("j'utilise une souris");
+        break;
+
+    case 'clavier':
+        alert("J'utilise un clavier");
+        break;
+
+    case 'ordinateur':
+        alert("J'utilise un ordinateur");
+        break;
+
+    case 'tapis':
+        alert("J'utilise un tapis");
+        break;
+
+    case 'imprimante':
+        alert("J'utilise une imprimante")
+        break;
+
+    case 'ondulateur':
+        alert("J'utilise un ondulateur")
+        break;
+
+    default:
+        alert("L'accessoire est inconnu")
+}
